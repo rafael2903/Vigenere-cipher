4d5913214b2dcf241199b0c4ab209e263c88b099
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2023-09-30 15:49:50 -0300

    implementa a decifração

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,20 +1,23 @@
 class VigenereCipher():
-    def __init__(self, key: str):
-        self.key = key.lower()
 
-    def encode(self, plain_text: str):
-        plain_text_length = len(plain_text)
-        key_length = len(self.key)
+    @staticmethod
+    def pad_key(key, text):
+        text_length = len(text)
+        key_length = len(key)
 
-        if key_length >= plain_text_length:
-            key_stream = self.key[:plain_text_length]
+        if key_length >= text_length:
+            key_stream = key[:text_length]
         else:
-            key_full_repetitions = plain_text_length // key_length
-            key_remaining_letters = plain_text_length % key_length
-            key_stream = self.key * key_full_repetitions
-            key_stream += self.key[:key_remaining_letters]
+            key_full_repetitions = text_length // key_length
+            key_remaining_letters = text_length % key_length
+            key_stream = key * key_full_repetitions
+            key_stream += key[:key_remaining_letters]
+        return key_stream
 
+    @staticmethod
+    def encode(key, plain_text):
         cipher_text = ''
+        key_stream = VigenereCipher.pad_key(key, plain_text)
         i = 0
 
         for letter in plain_text:
@@ -26,15 +29,38 @@ class VigenereCipher():
             key_letter = key_stream[i]
             key_letter_position = ord(key_letter) - ord('a')
             letter_position = ord(letter) - base_char_code
+
             cipher_letter_position = (
                 letter_position + key_letter_position) % 26
             cipher_letter = chr(base_char_code + cipher_letter_position)
+
             cipher_text += cipher_letter
             i += 1
 
         return cipher_text
 
-    def decode(self, cipher_text: str):
-        pass
+    @staticmethod
+    def decode(key, cipher_text):
+        key_stream = VigenereCipher.pad_key(key, cipher_text)
+        plain_text = ''
+        i = 0
 
+        for letter in cipher_text:
+            if not letter.isalpha() or not letter.isascii():
+                plain_text += letter
+                continue
 
+            base_char_code = ord('a') if letter.islower() else ord('A')
+            key_letter = key_stream[i]
+            key_letter_position = ord(key_letter) - ord('a')
+            letter_position = ord(letter) - base_char_code
+
+            plain_letter_position = letter_position - key_letter_position
+            if plain_letter_position < 0:
+                plain_letter_position += 26
+
+            plain_letter = chr(base_char_code + plain_letter_position)
+            plain_text += plain_letter
+            i += 1
+
+        return plain_text
--- tests.py
+++ tests.py
@@ -5,32 +5,37 @@ from VigenereCipher import VigenereCipher
 
 class Test(unittest.TestCase):
     key = 'mykey'
-    cipher = VigenereCipher(key)
 
     @classmethod
     def setUpClass(self):
+        self.simple_plain_text = 'teste testando o texto de teste para o trabalho'
+        self.simple_cipher_text = 'fccxc fccxyzby s rqvds bq rowrq nkvy a rbezmjrs'
+
         self.plain_text_file = open('plain_text_test.txt', 'r')
-        self.plain_text = self.plain_text_file.read()
+        self.complex_plain_text = self.plain_text_file.read()
         self.cipher_text_file = open('cipher_text_test.txt', 'r')
-        self.cipher_text = self.cipher_text_file.read()
+        self.complex_cipher_text = self.cipher_text_file.read()
+
+    def test_simple_encode(self):
+        cipher_text = VigenereCipher.encode(self.key, self.simple_plain_text)
+        self.assertEqual(cipher_text, self.simple_cipher_text)
+
+    def test_complex_encode(self):
+        cipher_text = VigenereCipher.encode(self.key, self.complex_plain_text)
+        self.assertEqual(cipher_text, self.complex_cipher_text)
+
+    def test_simple_decode(self):
+        plain_text = VigenereCipher.decode(self.key, self.simple_cipher_text)
+        self.assertEqual(plain_text, self.simple_plain_text)
+
+    def test_complex_decode(self):
+        plain_text = VigenereCipher.decode(self.key, self.complex_cipher_text)
+        self.assertEqual(plain_text, self.complex_plain_text)
 
     @classmethod
     def tearDownClass(self):
         self.plain_text = self.plain_text_file.close()
         self.cipher_text = self.cipher_text_file.close()
-
-    def test_encode1(self):
-        plain_text = 'teste testando o texto de teste para o trabalho'
-        cipher_text = 'fccxc fccxyzby s rqvds bq rowrq nkvy a rbezmjrs'
-        self.assertEqual(self.cipher.encode(plain_text), cipher_text)
-
-    def test_encode2(self):
-        self.assertEqual(self.cipher.encode(self.plain_text), self.cipher_text)
-
-    def test_decode(self):
-        plain_text = 'teste testando o texto de teste para o trabalho'
-        cipher_text = 'fccxc fccxyzby s rqvds bq rowrq nkvy a rbezmjrs'
-        self.assertEqual(self.cipher.decode(cipher_text), plain_text)
 
 
 if __name__ == '__main__':
