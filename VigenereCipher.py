2bdd35b99285ee16665c7b4b11e78658619afd39
Rafael Rodrigues <rafaelrodrigues2903@gmail.com>
2023-09-30 22:41:41 -0300

    implementa a quebra da cifra

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,56 +1,190 @@
-class VigenereCipher():
+import re
+from collections import Counter
+from enum import Enum
+
+
+class Mode(Enum):
     ENCODE = 'encode'
     DECODE = 'decode'
 
-    @staticmethod
-    def encode(key: str, plain_text: str):
-        return VigenereCipher.translate(key, plain_text, VigenereCipher.ENCODE)
 
-    @staticmethod
-    def decode(key: str, cipher_text: str):
-        return VigenereCipher.translate(key, cipher_text, VigenereCipher.DECODE)
+class Language(Enum):
+    ENGLISH = 'english'
+    PORTUGUESE = 'portuguese'
 
-    @staticmethod
-    def pad_key(key: str, text: str):
-        key = key.lower()
-        text_length = len(text)
-        key_length = len(key)
 
-        if key_length >= text_length:
-            key_stream = key[:text_length]
+letters_frequencies = {
+    Language.ENGLISH: {
+        'a': 0.0817, 'b': 0.0149, 'c': 0.0278, 'd': 0.0425,
+        'e': 0.1270, 'f': 0.0223, 'g': 0.0202, 'h': 0.0609,
+        'i': 0.0697, 'j': 0.0015, 'k': 0.0077, 'l': 0.0403,
+        'm': 0.0241, 'n': 0.0675, 'o': 0.0751, 'p': 0.0193,
+        'q': 0.0010, 'r': 0.0599, 's': 0.0633, 't': 0.0906,
+        'u': 0.0276, 'v': 0.0098, 'w': 0.0236, 'x': 0.0015,
+        'y': 0.0197, 'z': 0.0007
+    },
+    Language.PORTUGUESE: {
+        'a': 0.1463, 'b': 0.0104, 'c': 0.0388, 'd': 0.0499,
+        'e': 0.1257, 'f': 0.0102, 'g': 0.0130, 'h': 0.0128,
+        'i': 0.0618, 'j': 0.0040, 'k': 0.0002, 'l': 0.0278,
+        'm': 0.0474, 'n': 0.0505, 'o': 0.1073, 'p': 0.0252,
+        'q': 0.0120, 'r': 0.0653, 's': 0.0781, 't': 0.0434,
+        'u': 0.0463, 'v': 0.0167, 'w': 0.0001, 'x': 0.0021,
+        'y': 0.0001, 'z': 0.0047,
+    }
+}
+
+
+def encode(key: str, plain_text: str):
+    return translate(key, plain_text, Mode.ENCODE)
+
+
+def decode(key: str, cipher_text: str):
+    return translate(key, cipher_text, Mode.DECODE)
+
+
+def pad_key(key: str, text: str):
+    key = key.lower()
+    text_length = len(text)
+    key_length = len(key)
+
+    if key_length >= text_length:
+        key_stream = key[:text_length]
+    else:
+        key_full_repetitions = text_length // key_length
+        key_remaining_letters = text_length % key_length
+        key_stream = key * key_full_repetitions
+        key_stream += key[:key_remaining_letters]
+    return key_stream
+
+
+def is_valid_letter(letter):
+    return letter.isalpha() and letter.isascii()
+
+
+def translate(key: str, text: str, mode: str):
+    key_stream = pad_key(key, text)
+    translated_text = ''
+    i = 0
+
+    for letter in text:
+        if not is_valid_letter(letter):
+            translated_text += letter
+            continue
+
+        base_char_code = ord('a') if letter.islower() else ord('A')
+        key_letter = key_stream[i]
+        key_letter_position = ord(key_letter) - ord('a')
+        letter_position = ord(letter) - base_char_code
+
+        if mode is Mode.ENCODE:
+            translated_letter_position = (
+                letter_position + key_letter_position) % 26
         else:
-            key_full_repetitions = text_length // key_length
-            key_remaining_letters = text_length % key_length
-            key_stream = key * key_full_repetitions
-            key_stream += key[:key_remaining_letters]
-        return key_stream
+            translated_letter_position = (
+                letter_position - key_letter_position) % 26
 
-    @staticmethod
-    def translate(key: str, text: str, mode: str):
-        key_stream = VigenereCipher.pad_key(key, text)
-        translated_text = ''
-        i = 0
+        translated_letter = chr(
+            base_char_code + translated_letter_position)
+        translated_text += translated_letter
+        i += 1
 
-        for letter in text:
-            if not letter.isalpha() or not letter.isascii():
-                translated_text += letter
-                continue
+    return translated_text
 
-            base_char_code = ord('a') if letter.islower() else ord('A')
-            key_letter = key_stream[i]
-            key_letter_position = ord(key_letter) - ord('a')
-            letter_position = ord(letter) - base_char_code
 
-            if mode is VigenereCipher.ENCODE:
-                translated_letter_position = (
-                    letter_position + key_letter_position) % 26
-            else:
-                translated_letter_position = (
-                    letter_position - key_letter_position) % 26
+def find_repeated_groups(text: str):
+    repeated_groups = dict()
 
-            translated_letter = chr(
-                base_char_code + translated_letter_position)
-            translated_text += translated_letter
-            i += 1
+    for i in range(len(text)):
+        if i + 2 > len(text):
+            break
+        search_string = text[i:i+2]
+        if search_string not in repeated_groups:
+            matches_index = [m.start()
+                             for m in re.finditer(search_string, text)]
+            if len(matches_index) >= 2:
+                repeated_groups[search_string] = matches_index
 
-        return translated_text
+    return repeated_groups
+
+
+def find_key_length(cipher_text: str):
+    repeated_groups = find_repeated_groups(cipher_text)
+    matches_index = repeated_groups.values()
+    distances = [b - a for indexes in matches_index for a,
+                 b in zip(indexes, indexes[1:])]
+
+    def find_divisors(number):
+        return [i for i in range(2, number + 1) if number % i == 0]
+
+    divisor_counts = Counter()
+    for distance in distances:
+        divisor_counts.update(find_divisors(distance))
+
+    most_common_divisors = divisor_counts.most_common(3)
+
+    if len(most_common_divisors):
+        key_length, _count = max(
+            most_common_divisors, key=lambda x: (x[1], x[0]))
+        return key_length
+
+
+def calculate_letter_frequencies(text):
+    letter_count = Counter(text)
+    total_letters = len(text)
+    frequencies = {letter: count / total_letters for letter,
+                   count in letter_count.items()}
+    return frequencies
+
+
+def discover_caesar_shift(cipher_text, language: Language):
+    cipher_text_frequencies = calculate_letter_frequencies(
+        cipher_text)
+
+    best_shift = None
+    best_score = float('inf')
+
+    for shift in range(26):
+        shifted_frequencies = {}
+        for letter, frequency in cipher_text_frequencies.items():
+            shifted_letter = chr(
+                ((ord(letter) - ord('a') - shift) % 26) + ord('a'))
+            shifted_frequencies[shifted_letter] = frequency
+
+        score = sum((shifted_frequencies.get(letter, 0) - english_frequency)**2
+                    for letter, english_frequency in letters_frequencies[language].items())
+
+        if score < best_score:
+            best_score = score
+            best_shift = shift
+    return best_shift
+
+
+def discover_key(cipher_text: str, language=Language.ENGLISH):
+    cleaned_cipher_text: str = (''.join(filter(
+        is_valid_letter, cipher_text))).lower()
+
+    key_length = find_key_length(cleaned_cipher_text)
+
+    if not key_length:
+        return
+
+    caesar_cipher_texts = ['' for i in range(key_length)]
+
+    for i in range(0, len(cleaned_cipher_text), key_length):
+        for j in range(key_length):
+            if i + j < len(cleaned_cipher_text):
+                caesar_cipher_texts[j] += cleaned_cipher_text[i+j]
+
+    key_shifts = [discover_caesar_shift(text, language)
+                  for text in caesar_cipher_texts]
+
+    key = ''.join(map(lambda shift: chr((ord('a') + shift)), key_shifts))
+    return key
+
+
+def break_cipher(cipher_text: str, language=Language.ENGLISH):
+    key = discover_key(cipher_text, language)
+    if not key:
+        return
+    return decode(key, cipher_text)
--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from VigenereCipher import VigenereCipher
+from VigenereCipher import Language, break_cipher, decode, discover_key, encode
 
 
 class Test(unittest.TestCase):
@@ -8,8 +8,8 @@ class Test(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        self.simple_plain_text = 'teste testando o texto de teste para o trabalho'
-        self.simple_cipher_text = 'fccxc fccxyzby s rqvds bq rowrq nkvy a rbezmjrs'
+        self.simple_plain_text = 'A frequencia de letras em um texto tem sido frequentemente estudada para uso em criptografia e analise de frequencia em particular'
+        self.simple_cipher_text = 'M dbiogcxggm bo pcfpkw cy sw xcjry xcy qshm rpousqldikqldi cerehypy zepm scs cy abmnfmqvyrgk i yzyvmqq bo jpqoeilogk ik bybxgosvep'
 
         self.plain_text_file = open('plain_text_test.txt', 'r')
         self.complex_plain_text = self.plain_text_file.read()
@@ -17,19 +17,30 @@ class Test(unittest.TestCase):
         self.complex_cipher_text = self.cipher_text_file.read()
 
     def test_simple_encode(self):
-        cipher_text = VigenereCipher.encode(self.key, self.simple_plain_text)
+        cipher_text = encode(self.key, self.simple_plain_text)
         self.assertEqual(cipher_text, self.simple_cipher_text)
 
     def test_complex_encode(self):
-        cipher_text = VigenereCipher.encode(self.key, self.complex_plain_text)
+        cipher_text = encode(self.key, self.complex_plain_text)
         self.assertEqual(cipher_text, self.complex_cipher_text)
 
     def test_simple_decode(self):
-        plain_text = VigenereCipher.decode(self.key, self.simple_cipher_text)
+        plain_text = decode(self.key, self.simple_cipher_text)
         self.assertEqual(plain_text, self.simple_plain_text)
 
     def test_complex_decode(self):
-        plain_text = VigenereCipher.decode(self.key, self.complex_cipher_text)
+        plain_text = decode(self.key, self.complex_cipher_text)
+        self.assertEqual(plain_text, self.complex_plain_text)
+
+    def test_discover_key(self):
+        self.assertEqual(discover_key(self.complex_cipher_text), self.key)
+
+    def test_discover_key_portuguese(self):
+        self.assertEqual(discover_key(self.simple_cipher_text,
+                                      Language.PORTUGUESE), self.key)
+
+    def test_break_cipher(self):
+        plain_text = break_cipher(self.complex_cipher_text, Language.PORTUGUESE)
         self.assertEqual(plain_text, self.complex_plain_text)
 
     @classmethod
